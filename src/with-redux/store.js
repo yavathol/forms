6c1ac16c8559047f8438680d9f2b49f98e205937
desfero <pavloblack@hotmail.com>
2018-03-04 19:28:54 +0100

    Live coding starting point

--- src/with-redux/store.js
+++ src/with-redux/store.js
@@ -1,23 +1,7 @@
-import {FORM_RESET, FORM_UPDATE_VALUE} from './actions';
-
-const initialState = {
-    contactForm: {}
-};
+const initialState = {};
 
 export const store = (state = initialState, action) => {
     switch (action.type) {
-
-        case FORM_UPDATE_VALUE:
-            return {
-                ...state,
-                contactForm: {
-                    ...state.contactForm,
-                    [action.payload.name]: action.payload.value,
-                }
-            };
-
-        case FORM_RESET:
-            return initialState;
 
         default:
             return state;
